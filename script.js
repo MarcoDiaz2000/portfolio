fa6d8cc758e8119ac88732e20711565c793644f6
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-11 03:14:04 -0600

    Fix linter

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".mainmenu");
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.mainmenu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  menu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
 });
 
-document.querySelectorAll(".menu-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    menu.classList.remove("active");
+document.querySelectorAll('.menu-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menu.classList.remove('active');
   })
 );
