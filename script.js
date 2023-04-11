938e198ecd5cd6f2c40372d7cdd6ff704fb2200d
ragan <raganalemu3@gmail.com>
2023-04-11 10:49:49 +0300

    Add JS file

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".mainmenu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+document.querySelectorAll(".menu-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+  })
+);
