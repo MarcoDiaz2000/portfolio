6c182d671966e74b8d493c4244e0374b73a45ee1
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-12 12:44:17 -0600

    Add modal and fix CSS

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ document.querySelectorAll(".menu-link").forEach((n) =>
 const projects = [
   {
     title: "Tonic",
-    devs: ["CANOPY • Back End Dev • 2015"],
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     image: "images/p1.png",
@@ -26,7 +25,6 @@ const projects = [
   },
   {
     title: "Multi-Post Stories",
-    devs: ["CANOPY • Back End Dev • 2015"],
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     image: "images/p2.png",
@@ -37,7 +35,6 @@ const projects = [
 
   {
     title: "Tonic",
-    devs: ["CANOPY • Back End Dev • 2015"],
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     image: "images/p3.png",
@@ -47,7 +44,6 @@ const projects = [
   },
   {
     title: "Multi-Post Stories",
-    devs: ["CANOPY • Back End Dev • 2015"],
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     image: "images/p4.png",
@@ -83,8 +79,64 @@ for (let i = 0; i < projects.length; i += 1) {
           <li class="wbtn3">${projects[i].javascript}</li>
         </ul>
       </div>
-      <button id="btnnum${i}" class="btn">See project</button>
+      <button class="btn">See project</button>
     </div>
   </div>
   `;
+}
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    displayProject(projects[index]);
+  });
+});
+
+function displayProject(project) {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  modal.innerHTML = `
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <h2 class="h2-modal">${project.title}</h2>
+      <div class="type type-modal">
+        <span class="texttype">CANOPY</span>
+        <img src="images/dot.png" alt="dot" />
+        <span class="texttype2">Back End Dev</span>
+        <img src="images/dot.png" alt="dot" />
+        <span class="texttype2">2015</span>
+      </div>
+      <img class="img-hero" src="${project.image}" alt="image-project" />
+      <div class="project-info">
+        <div class="column1">
+          <div class="description">
+            <p class="text1-modal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          </div>
+        </div>
+        <div class="column2">
+          <div class="buttons">
+            <ul class="wbtn1">
+              <li class="wbtn3">${project.html}</li>
+              <li class="wbtn3">${project.css}</li>
+              <li class="wbtn3">${project.javascript}</li>
+            </ul>
+            <hr>
+            <div class="button-container">
+              <button class="btn-modal">See sive<img class="icon" src="images/iconlive.png" alt="github" /></button>
+              <button class="btn-modal">See source<img class="icon" src="images/icongithub.png" alt="github" /></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div><p>.</p></div>
+    </div>
+  `;
+
+  const closeButton = modal.querySelector('.close');
+  closeButton.addEventListener('click', () => {
+    modal.remove();
+  });
+
+  document.body.appendChild(modal);
 }
