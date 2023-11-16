a73de2033e8e33cbbdf08f7b5f7851f087c38c33
Marco Díaz <md.marcodiaz2000@gmail.com>
2023-11-16 04:22:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const projects = [
   {
     title: 'Star Wars Movies',
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      'The JavaScript capstone project is about building your own web application based on an external API. You will select an API that provides data about a topic that you like and then build the webapp around it. The webapp will have 2 user interfaces: A home page showing a list of items that you can "like." A popup window with more data about an item',
     description2:
       '"Star Wars Movies" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. You will build a simple website that allows for doing that, and you will do it using ES6 and Webpack!',
     image: 'images/p4.png',
